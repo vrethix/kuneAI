c16fd7ffe0b0e150aee746d46ed40d904bf7e821
Marko Bilal <marko.bilal@hey.com>
2025-01-20 10:19:49 +0100

    adds details to InsightData and a schema description string

--- agent/src/services/tokenAnalysis.ts
+++ agent/src/services/tokenAnalysis.ts
@@ -4,13 +4,25 @@ import { baseUrls, COINGECKO_API_KEY, GOLD_RUSH_API_KEY } from "./constants";
 
 export type ITokenAnalysisService = Service & {
     generateInsightData(symbol: string): Promise<InsightData | string>;
+    getInsightDataSchema(): string;
 };
 
 export type InsightData = {
     chain: string;
     metrics: {
-        holders: any;
-        prices: any;
+        holders: {
+            numCurrentHolders: number;
+            numNewHolders24h: number;
+            numHolders24hChange: number;
+            numHolders24hChangePercentage: number;
+        };
+        prices: {
+            currentPrice: number;
+            priceChange24h: number;
+            percentageChange24h: number;
+            marketCap: number;
+            marketCapChange24hPercentage: number;
+        };
     };
 };
 
@@ -42,7 +54,42 @@ class TokenAnalysisService extends Service {
     public async generateInsightData(
         symbol: string
     ): Promise<InsightData | string> {
-        return "fake stuff";
+        return {
+            chain: "ETH",
+            metrics: {
+                holders: {
+                    numCurrentHolders: 1000000,
+                    numNewHolders24h: 1000000,
+                    numHolders24hChange: 1000000,
+                    numHolders24hChangePercentage: 1000000,
+                },
+                prices: {
+                    currentPrice: 1200,
+                    priceChange24h: 1000,
+                    percentageChange24h: 20,
+                    marketCap: 1000000,
+                    marketCapChange24hPercentage: 5,
+                },
+            },
+        };
+    }
+
+    public getInsightDataSchema(): string {
+        return `
+        InsightData Schema:
+        - chain: string (The blockchain network, e.g., "ETH", "BSC")
+        - metrics:
+        - holders:
+            - numCurrentHolders: number (Total current holders of the token)
+            - numNewHolders24h: number (New holders in the last 24 hours)
+            - numHolders24hChange: number (Absolute change in holders over 24 hours)
+            - numHolders24hChangePercentage: number (Percentage change in holders over 24 hours)
+        - prices:
+            - currentPrice: number (Current token price in USD)
+            - priceChange24h: number (Absolute price change in last 24 hours)
+            - percentageChange24h: number (Percentage price change in last 24 hours)
+            - marketCap: number (Current market capitalization in USD)
+            - marketCapChange24hPercentage: number (Percentage change in market cap over 24 hours)`;
     }
 }
 
